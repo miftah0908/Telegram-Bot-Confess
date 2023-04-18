144a06c86ccc5f99ac416115645d4656a62eab27
Miftah0908 <99408827+miftah0908@users.noreply.github.com>
2023-04-19 02:29:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6008084117:AAF-YwmcYEhieV3GjmpqaO7tP4REQQ1lHVE';
-const bot = new TelegramBot(token, { polling: true });
 
-// Command "/start" untuk memberikan pesan awal ketika bot dinyalakan
+const botToken = '6008084117:AAF-YwmcYEhieV3GjmpqaO7tP4REQQ1lHVE';
+const bot = new TelegramBot(botToken, { polling: true });
+
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Halo! Aku bot confess, yang akan membantumu mengirim pesan rahasia ke temanmu. Ketik "/confess" untuk mengirim pesan rahasia.');
+  bot.sendMessage(msg.chat.id, 'Halo, selamat datang di bot confess!');
 });
 
-// Command "/confess" untuk memulai proses confessing
 bot.onText(/\/confess/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Ketik username temanmu dan pesan yang ingin kamu kirimkan. Contoh: \n\n @username_teman Pesan rahasiamu di sini.');
+  const chatId = msg.chat.id;
+  const opts = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Kirim pesan',
+            callback_data: 'send_message',
+          },
+          {
+            text: 'Batal',
+            callback_data: 'cancel',
+          },
+        ],
+      ],
+    },
+  };
+  bot.sendMessage(chatId, 'Silakan pilih aksi:', opts);
 });
 
-// Menangkap pesan dari pengguna
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const message = msg.text;
-
-  // Memeriksa apakah pesan mengandung username teman dan teks confessing
-  if (/^@([A-Za-z0-9_]{5,})\s(.+)/.test(message)) {
-    // Menemukan username dan pesan
-    const username = message.match(/^@([A-Za-z0-9_]{5,})\s(.+)/)[1];
-    const confessMessage = message.match(/^@([A-Za-z0-9_]{5,})\s(.+)/)[2];
-
-    // Mengirim pesan confessing ke teman
-    bot.sendMessage(username, `Kamu mendapat pesan confess dari ${msg.from.username}: ${confessMessage}`, { reply_markup: { inline_keyboard: [[{ text: 'Balas Pesan', callback_data: `reply_${msg.from.id}` }]] } })
-      .then(() => {
-        bot.sendMessage(msg.chat.id, `Pesanmu telah terkirim ke @${username}.`);
-      })
-      .catch((error) => {
-        bot.sendMessage(msg.chat.id, `Maaf, aku tidak dapat mengirim pesan ke @${username}. Pastikan username tersebut benar dan kamu sudah berteman dengannya.`);
-      });
+bot.on('callback_query', (callbackQuery) => {
+  const message = callbackQuery.message;
+  if (callbackQuery.data === 'send_message') {
+    const chatId = message.chat.id;
+    bot.sendMessage(chatId, 'Silakan ketikkan pesanmu untuk confes di sini:');
+  } else if (callbackQuery.data === 'cancel') {
+    bot.sendMessage(message.chat.id, 'Batal');
   }
 });
 
-// Menangkap respon dari inline keyboard
-bot.on('callback_query', (callbackQuery) => {
-  const data = callbackQuery.data;
-  const chatId = callbackQuery.message.chat.id;
-  const messageId = callbackQuery.message.message_id;
-
-  // Memeriksa apakah respon adalah permintaan balasan
-  if (/^reply_/.test(data)) {
-    const userId = data.split('_')[1];
-    bot.sendMessage(chatId, `Ketik pesan yang ingin kamu balas ke @${bot.users[userId].username}.`, { reply_to_message_id: messageId });
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  if (msg.text.startsWith('/confess')) {
+    const confessMessage = msg.text.substring(8);
+    bot.sendMessage(chatId, 'Terima kasih, pesanmu telah terkirim!');
+    bot.sendMessage(chatId, `Pesan confes kamu: ${confessMessage}`);
   }
 });
